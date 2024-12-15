1edacbd22cb4799b461f1495f6978ede99133444
Jonny Comes <jcomes@collegeofidaho.edu>
2024-12-15 16:50:02 -0700

    Setting up variable declaration.

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,13 +13,18 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         
-        defineAst(outputDir, "Stmt", Arrays.asList(
-            "Expression : Expr expression",
-                 "Print      : Expr expression"
-        //    "Binary   : Expr left, Token operator, Expr right",
-        //         "Grouping : Expr expression",
-        //         "Literal  : Object value",
-        //         "Unary    : Token operator, Expr right"
+        defineAst(outputDir, "Expr", Arrays.asList(
+                // baseName: Stmt
+            // "Expression : Expr expression", 
+            //      "Print      : Expr expression",
+            //      "Var        : Token name, Expr initializer"
+
+                // baseName: Expr
+           "Binary   : Expr left, Token operator, Expr right", 
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right",
+                "Variable : Token name"
         ));
     }
 
