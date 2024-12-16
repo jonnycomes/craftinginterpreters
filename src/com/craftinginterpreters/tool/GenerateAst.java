0d3e0d7d1f0edc04a1fc46082b66df94ed89cf83
Jonny Comes <jcomes@collegeofidaho.edu>
2024-12-16 15:50:42 -0700

    Adding while functionality.

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,23 +13,24 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         
-        defineAst(outputDir, "Expr", Arrays.asList(
+        defineAst(outputDir, "Stmt", Arrays.asList(
                 // baseName: Stmt
-            // "Block      : List<Stmt> statements",
-            //      "Expression : Expr expression", 
-            //      "If         : Expr condition, Stmt thenBranch," +
-            //                  " Stmt elseBranch",
-            //      "Print      : Expr expression",
-            //      "Var        : Token name, Expr initializer"
+            "Block      : List<Stmt> statements",
+                 "Expression : Expr expression", 
+                 "If         : Expr condition, Stmt thenBranch," +
+                             " Stmt elseBranch",
+                 "Print      : Expr expression",
+                 "Var        : Token name, Expr initializer",
+                 "While      : Expr condition, Stmt body"
 
                 // baseName: Expr
-           "Assign   : Token name, Expr value",
-                "Binary   : Expr left, Token operator, Expr right", 
-                "Grouping : Expr expression",
-                "Literal  : Object value",
-                "Logical  : Expr left, Token operator, Expr right",
-                "Unary    : Token operator, Expr right",
-                "Variable : Token name"
+        //    "Assign   : Token name, Expr value",
+        //         "Binary   : Expr left, Token operator, Expr right", 
+        //         "Grouping : Expr expression",
+        //         "Literal  : Object value",
+        //         "Logical  : Expr left, Token operator, Expr right",
+        //         "Unary    : Token operator, Expr right",
+        //         "Variable : Token name"
         ));
     }
 
