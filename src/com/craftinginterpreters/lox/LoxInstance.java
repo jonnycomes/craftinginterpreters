a717ab99baef3dc0c287d19e218b8108a6b104a0
Jonny Comes <jcomes@collegeofidaho.edu>
2024-12-21 08:08:06 -0700

    Adding get expressions.

--- src/com/craftinginterpreters/lox/LoxInstance.java
+++ src/com/craftinginterpreters/lox/LoxInstance.java
@@ -1,10 +1,23 @@
 package com.craftinginterpreters.lox;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class LoxInstance {
     private LoxClass klass;
+    private final Map<String, Object> fields = new HashMap<>();
     
     LoxInstance(LoxClass klass) {
         this.klass = klass;
+    }
+
+    Object get(Token name) {
+        if (fields.containsKey(name.lexeme)) {
+            return fields.get(name.lexeme);
+        }
+
+        throw new RuntimeError(name, 
+            "Undfined property '" + name.lexeme + "'.");
     }
 
     @Override
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,29 +13,30 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         
-        defineAst(outputDir, "Stmt", Arrays.asList(
+        defineAst(outputDir, "Expr", Arrays.asList(
                 // baseName: Stmt
-            "Block      : List<Stmt> statements",
-                 "Class      : Token name, List<Stmt.Function> methods",
-                 "Expression : Expr expression", 
-                 "Function   : Token name, List<Token> params," +
-                             " List<Stmt> body",
-                 "If         : Expr condition, Stmt thenBranch," +
-                             " Stmt elseBranch",
-                 "Print      : Expr expression",
-                 "Return     : Token keyword, Expr value",
-                 "Var        : Token name, Expr initializer",
-                 "While      : Expr condition, Stmt body"
+            // "Block      : List<Stmt> statements",
+            //      "Class      : Token name, List<Stmt.Function> methods",
+            //      "Expression : Expr expression", 
+            //      "Function   : Token name, List<Token> params," +
+            //                  " List<Stmt> body",
+            //      "If         : Expr condition, Stmt thenBranch," +
+            //                  " Stmt elseBranch",
+            //      "Print      : Expr expression",
+            //      "Return     : Token keyword, Expr value",
+            //      "Var        : Token name, Expr initializer",
+            //      "While      : Expr condition, Stmt body"
 
                 // baseName: Expr
-        //    "Assign   : Token name, Expr value",
-        //         "Binary   : Expr left, Token operator, Expr right", 
-        //         "Call     : Expr callee, Token paren, List<Expr> arguments",
-        //         "Grouping : Expr expression",
-        //         "Literal  : Object value",
-        //         "Logical  : Expr left, Token operator, Expr right",
-        //         "Unary    : Token operator, Expr right",
-        //         "Variable : Token name"
+           "Assign   : Token name, Expr value",
+                "Binary   : Expr left, Token operator, Expr right", 
+                "Call     : Expr callee, Token paren, List<Expr> arguments",
+                "Get      : Expr object, Token name",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Logical  : Expr left, Token operator, Expr right",
+                "Unary    : Token operator, Expr right",
+                "Variable : Token name"
         ));
     }
 
